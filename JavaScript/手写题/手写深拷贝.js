5ec11d3269eb024b273efec377d104d4926eeb96
hasmokan <1021056159@qq.com>
2023-11-16 23:34:41 +0800

    ✨ feat: 手写bindapplycall拍平new

---
+++ JavaScript/手写题/手写深拷贝.js
@@ -0,0 +1,53 @@
+const obj = {
+	arr: [1, 2, 3],
+	a: 4,
+};
+obj.sub = obj;
+obj.arr.push(obj);
+
+function deepClone(obj = {}) {
+	const map = new WeakMap();
+
+	const copy = (value) => {
+		if (!value || typeof value !== "object") return value;
+
+		if (map.has(value)) {
+			return map.get(value);
+		}
+
+		let result = Array.isArray(value) ? [] : {};
+		map.set(value, result);
+
+		for (let key in value) {
+			if (value.hasOwnProperty(key)) {
+				result[key] = copy(value);
+			}
+		}
+
+		return result;
+	};
+
+	return copy(obj);
+}
+
+let newObj = deepClone(obj);
+console.log(newObj.arr !== obj.arr);
+console.log(newObj.sub !== obj.sub);
+console.log(newObj.arr[3] !== obj);
+console.log(newObj.arr[3] !== newObj);
+
+/*手写无法处理循环的深拷贝*/
+// let deepClone = (obj = {}) => {
+// 	if (!obj || typeof obj !== "object") return;
+
+// 	let newObj = Array.isArray(obj) ? [] : {};
+
+// 	for (let key in obj) {
+// 		// 判断是否是自身的属性而不是原型链上的属性
+// 		if (obj.hasOwnProperty(key)) {
+// 			newObj[key] =
+// 				typeof obj[key] === "object" ? deepClone(obj[key]) : obj[key];
+// 		}
+// 	}
+// 	return newObj;
+// };
--- JavaScript/手写题/数组拍平.js
+++ JavaScript/手写题/数组拍平.js
@@ -1,36 +1,19 @@
 /*数组拍平可以按照n层深度拍平*/
-let arr = [1, 2, 3, [4, 5, 6], [7, 8, [9, 10, 11], 12], [13, 14, 15]]
-let n = 0
+let arr = [1, 2, 3, [4, 5, 6], [7, 8, [9, 10, 11], 12], [13, 14, 15]];
+let n = 0;
 
 /*string*/
-// return arr.toString().split(',');
-
-/*递归*/
-function flatten(arr){
-    let result = []
-    for(let i = 0; i < arr.length; i++){
-        if(Array.isArray(arr[i])){
-            result = result.concat(flatten(arr[i]))
-        }   
-        else{
-            result.push(arr[i])
-        }
-    }
-    return result
-}
+return arr.toString().split(",");
 
 /*for*/
-function flatten(arr){
-    let result = []
-    for(let i = 0; i < arr.length; i++){
-        if(Array.isArray(arr[i])){
-            result = result.concat(flatten(arr[i]))
-        }   
-        else{
-            result.push(arr[i])
-        }
-    }
-    return result
+function flatten(arr) {
+	let result = [];
+	for (let i = 0; i < arr.length; i++) {
+		if (Array.isArray(arr[i])) {
+			result = result.concat(flatten(arr[i]));
+		} else {
+			result.push(arr[i]);
+		}
+	}
+	return result;
 }
-
-console.log(flatten(arr))
