54b1aa541e6f88d191399f69b0e03ec1ae1e3efa
hasmokan <1021056159@qq.com>
2023-11-16 16:07:35 +0800

    feat

---
+++ JavaScript/手写题/数组拍平.js
@@ -0,0 +1,36 @@
+/*数组拍平可以按照n层深度拍平*/
+let arr = [1, 2, 3, [4, 5, 6], [7, 8, [9, 10, 11], 12], [13, 14, 15]]
+let n = 0
+
+/*string*/
+// return arr.toString().split(',');
+
+/*递归*/
+function flatten(arr){
+    let result = []
+    for(let i = 0; i < arr.length; i++){
+        if(Array.isArray(arr[i])){
+            result = result.concat(flatten(arr[i]))
+        }   
+        else{
+            result.push(arr[i])
+        }
+    }
+    return result
+}
+
+/*for*/
+function flatten(arr){
+    let result = []
+    for(let i = 0; i < arr.length; i++){
+        if(Array.isArray(arr[i])){
+            result = result.concat(flatten(arr[i]))
+        }   
+        else{
+            result.push(arr[i])
+        }
+    }
+    return result
+}
+
+console.log(flatten(arr))
