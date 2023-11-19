b6cbcbd294f2893d986105a74ca2c03a7aeb8205
hasmokan <1021056159@qq.com>
2023-11-19 23:21:41 +0800

    ✨ feat: 版本号排序 ip地址排序

--- JavaScript/函数柯里化.js
+++ JavaScript/函数柯里化.js
@@ -1,22 +1,31 @@
-function add(a, b, c, d) {
-  return a + b + c + d
-}
+// function add(a, b, c, d) {
+// 	return a + b + c + d;
+// }
 
+// function curry(fn) {
+// 	return function curried(...args) {
+// 		if (fn.length <= args.length) {
+// 			return fn.apply(this, args);
+// 		} else {
+// 			return function (...newArgs) {
+// 				return curried.apply(this, args.concat(newArgs));
+// 			};
+// 		}
+// 	};
+// }
 
-function curry(fn){
-  return function curried(...args){
-    if(fn.length <= args.length){
-      return fn.apply(this, args)
-    } else{
-      return function(...newArgs){
-        // 再次柯里化， 尾递归
-        return curried.apply(this, args.concat(newArgs))
-      }
-    }
-  }
-}
+// function curry(fn) {
+// 	return function curried(...args) {
+// 		if (fn.length <= args.length) {
+// 			return fn.apply(this, args);
+// 		} else {
+// 			return (...args2) => curried(...args, ...args2);
+// 		}
+// 	};
+// }
 
+// let curryAdd = curry(add);
 
-let curryAdd = curry(add)
+// console.log(curryAdd(1)(2)(3)(4));
 
-console.log(curryAdd(1)(2)(3)(4));
+console.log(typeof +"255");
---
+++ JavaScript/手写题/ip地址排序 版本号排序.js
@@ -0,0 +1,15 @@
+let arr = [];
+
+const versions = [
+	"0.5.1",
+	"0.1.1",
+	"2.3.3",
+	"0.302.1",
+	"4.2",
+	"4.3.5",
+	"4.3.4",
+];
+
+console.log(versions.sort());
+
+/* https://segmentfault.com/a/1190000040934683 */
