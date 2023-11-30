85250e5f444336f370c0721544836fb906360099
hasmokan <1021056159@qq.com>
2023-11-30 22:49:25 +0800

    ✨ feat: promisify

--- JavaScript/手写题/手写promisify.js
+++ JavaScript/手写题/手写promisify.js
@@ -8,3 +8,27 @@ function myPromisify(callback) {
 		});
 	};
 }
+
+// 假设这是一个基于回调的函数
+function asyncFunction(input, callback) {
+	// 异步操作
+	setTimeout(() => {
+		if (input) {
+			callback(null, `Success: ${input}`);
+		} else {
+			callback(new Error("Input is missing"));
+		}
+	}, 1000);
+}
+
+// 使用 myPromisify 转换为返回 Promise 的函数
+const promisedAsyncFunction = myPromisify(asyncFunction);
+
+// 现在可以像使用 Promise 的其他函数一样使用它
+promisedAsyncFunction("Hello")
+	.then((result) => {
+		console.log(result); // 输出: Success: Hello
+	})
+	.catch((error) => {
+		console.error(error); // 输出: Error: Input is missing
+	});
